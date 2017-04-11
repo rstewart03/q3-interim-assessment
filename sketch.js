6780e79843a68021f646022935c89c1e30679509
Rickya Stewart <rstewart@wlapcs.org>
2017-04-11 17:44:28 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup(){
+    createCanvas(600,400);
+    background(255, 56, 56);
+}
+ 
+ function draw(){
+     strokeWeight(4);
+     stroke(255);
+     
+     var x = 0;
+     while(x<650){
+         ellipse(255, 25, 25);
+         x = x + 50;
+     }
+     
+     
+ }
+    
